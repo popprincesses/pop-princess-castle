eeef5cb215709372a1875587450e93dd599792fa
Haley Bowes (LCL) <Haley.Bowes@loblaw.ca>
2019-02-20 11:28:53 -0500

    adding a router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
 
+var config = {
+	apiKey: "AIzaSyACzuLtBRZiUe_bLfLZnHd3ypdf07zVtlk",
+	authDomain: "pop-princesses.firebaseapp.com",
+	databaseURL: "https://pop-princesses.firebaseio.com",
+	projectId: "pop-princesses",
+	storageBucket: "pop-princesses.appspot.com",
+	messagingSenderId: "770430376686"
+};
+firebase.initializeApp(config);
 class App extends Component {
   render() {
     return (
